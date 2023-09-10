2551c5013485ebedbaeacefedcf0d5d6ede6ea9d
dlopes7 <davidribeirolopes@gmail.com>
2023-09-10 16:41:34 -0500

    more parsing

--- src/ast.zig
+++ src/ast.zig
@@ -10,23 +10,8 @@ pub const ASTParser = struct {
     allocator: Allocator,
     source: std.json.Reader(std.json.default_buffer_size, std.fs.File.Reader),
     state: ParserState = .root,
+    currentLevel: u32 = 0,
     const log = std.log.scoped(.astParser);
-
-    pub fn parse(allocator: Allocator, reader: std.fs.File.Reader) !void {
-        var timer = try std.time.Timer.start();
-
-        // The underlying reader, which already handles JSON
-        const source = std.json.reader(allocator, reader);
-
-        var parser = ASTParser{
-            .allocator = allocator,
-            .source = source,
-        };
-        while (try parser.next()) {}
-
-        const elapsed = timer.read();
-        log.debug("ASTParser.parse: {}us", .{elapsed / 1000});
-    }
 
     fn getString(self: *@This()) Error![]const u8 {
         const value = self.source.nextAlloc(self.allocator, .alloc_if_needed) catch |err| {
@@ -64,7 +49,7 @@ pub const ASTParser = struct {
     fn parseRoot(self: *@This(), s: []const u8) !void {
         if (std.mem.eql(u8, s, "name")) {
             const value = try self.getString();
-            log.debug("Found name {s}", .{value});
+            self.print("Found name {s}", .{value});
         } else if (std.mem.eql(u8, s, "expression")) {
             self.state = .parsing;
         }
@@ -78,27 +63,29 @@ pub const ASTParser = struct {
     }
 
     fn parseLoc(self: *@This()) Error!void {
+        var loc: spec.Loc = undefined;
         while (true) {
             const token = try self.getSourceNext();
             switch (token) {
                 .object_begin => {
+                    // self.print("parseLoc - object_begin", .{});
+                    self.currentLevel += 1;
                     continue;
                 },
                 .object_end => {
+                    self.currentLevel -= 1;
+                    // self.print("parseLoc - object_end", .{});
                     break;
                 },
                 .string => |value| {
                     if (std.mem.eql(u8, value, "start")) {
-                        const start = try self.getU32();
-                        log.debug("Location(start={d})", .{start});
+                        loc.start = try self.getU32();
                     }
                     if (std.mem.eql(u8, value, "end")) {
-                        const end = try self.getU32();
-                        log.debug("Location(end={d})", .{end});
+                        loc.end = try self.getU32();
                     }
                     if (std.mem.eql(u8, value, "filename")) {
-                        const filename = try self.getString();
-                        log.debug("Location(filename={s})", .{filename});
+                        loc.filename = try self.getString();
                     }
                 },
                 else => |err| {
@@ -107,6 +94,8 @@ pub const ASTParser = struct {
                 },
             }
         }
+
+        self.print("Loc(./{s}:{d}:{d})", .{ loc.filename, loc.start, loc.end });
     }
 
     fn parseParameter(self: *@This()) Error!void {
@@ -114,15 +103,17 @@ pub const ASTParser = struct {
             const token = try self.getSourceNext();
             switch (token) {
                 .object_begin => {
+                    self.currentLevel += 1;
                     continue;
                 },
                 .object_end => {
+                    self.currentLevel -= 1;
                     return;
                 },
                 .string => |value| {
                     if (std.mem.eql(u8, value, "text")) {
                         const text = try self.getString();
-                        log.debug("Parameter(text={s})", .{text});
+                        self.print("Parameter(text={s})", .{text});
                     }
                     if (std.mem.eql(u8, value, "location")) {
                         try self.parseLoc();
@@ -143,7 +134,7 @@ pub const ASTParser = struct {
 
         // if (std.mem.eql(u8, value, "text")) {
         //     const text = try self.getString();
-        //     log.debug("Parameter(text={s})", .{text});
+        //     self.print("Parameter(text={s})", .{text});
         // }
     }
 
@@ -152,7 +143,7 @@ pub const ASTParser = struct {
             const token = try self.getSourceNext();
             switch (token) {
                 .object_end => {
-                    log.debug("parseLet - Found object_end", .{});
+                    self.currentLevel -= 1;
                     return;
                 },
                 .string => |value| {
@@ -173,59 +164,183 @@ pub const ASTParser = struct {
             }
         }
     }
+    fn parseBinaryOp(self: *@This()) Error!void {
+        const value = try self.getString();
+        self.print("BinaryOp(value={s})", .{value});
+    }
 
-    fn parseTerm(self: *@This(), s: []const u8) Error!void {
-        const key = std.meta.stringToEnum(spec.ValidKeys, s) orelse {
-            log.err("Key {s} not in spec.ValidKeys", .{s});
-            return;
-        };
-        switch (key) {
-            .kind => {
-                const value = try self.getString();
-                log.debug("parseTerm(key={s}, kind={s})", .{ s, value });
-                const kind = std.meta.stringToEnum(spec.ValidTerms, value) orelse {
-                    log.err("Found an invalid term kind={s}", .{value});
+    fn parseBinary(self: *@This()) Error!void {
+        while (true) {
+            const token = try self.getSourceNext();
+
+            switch (token) {
+                .object_end => {
+                    self.currentLevel -= 1;
+                    // self.print("parseBinary - object_end", .{});
                     return;
-                };
-                switch (kind) {
-                    .Let => {
-                        try self.parseLet();
-                    },
-                    else => |k| {
-                        log.err("Not implemented for kind {any}", .{k});
-                    },
-                }
-            },
-            else => |k| {
-                log.err("Not implemented for key {any}", .{k});
-                return Error.ParserError;
-            },
+                },
+                .string => |value| {
+                    if (std.mem.eql(u8, value, "lhs")) {
+                        try self.parseTerm(value);
+                    } else if (std.mem.eql(u8, value, "op")) {
+                        try self.parseBinaryOp();
+                    } else if (std.mem.eql(u8, value, "rhs")) {
+                        try self.parseTerm(value);
+                    } else if (std.mem.eql(u8, value, "location")) {
+                        try self.parseLoc();
+                    }
+                },
+                else => |err| {
+                    log.err("parseBinary - expected string, found {any}", .{err});
+                    return Error.ParserError;
+                },
+            }
+        }
+    }
+
+    fn parseInt(self: *@This()) Error!void {
+        while (true) {
+            const token = try self.getSourceNext();
+            switch (token) {
+                .object_end => {
+                    self.currentLevel -= 1;
+                    // self.print("parseInt - object_end", .{});
+                    return;
+                },
+                .string => |value| {
+                    if (std.mem.eql(u8, value, "value")) {
+                        const number = try self.getU32();
+                        self.print("Int(value={d})", .{number});
+                    } else if (std.mem.eql(u8, value, "location")) {
+                        try self.parseLoc();
+                    }
+                },
+                else => |err| {
+                    log.err("parseInt - expected string, found {any}", .{err});
+                    return Error.ParserError;
+                },
+            }
+        }
+    }
+
+    fn print(self: *@This(), comptime format: []const u8, args: anytype) void {
+        for (0..self.currentLevel) |_| {
+            std.debug.print("  ", .{});
+        }
+        std.debug.print(format, args);
+        std.debug.print("\n", .{});
+    }
+
+    fn parseTerm(self: *@This(), owner: []const u8) Error!void {
+        _ = owner;
+        while (true) {
+            const token = try self.getSourceNext();
+
+            switch (token) {
+                .object_begin => {
+                    // self.print("parseTerm(owner={s}) object_begin", .{owner});
+                    self.currentLevel += 1;
+                    continue;
+                },
+                .object_end => {
+                    for (0..self.currentLevel) |_| {
+                        std.debug.print(" ", .{});
+                    }
+                    // self.print("parseTerm(owner={s}) object_end", .{owner});
+                    self.currentLevel -= 1;
+                    break;
+                },
+                .string => |value| {
+                    if (std.mem.eql(u8, value, "kind")) {
+                        const kind = try self.getString();
+
+                        // self.print("parseTerm(owner={s}, kind={s})", .{ owner, kind });
+
+                        const kindEnum = std.meta.stringToEnum(spec.ValidTerms, kind) orelse {
+                            log.err("Found an invalid term kind={s}", .{kind});
+                            return;
+                        };
+                        switch (kindEnum) {
+                            .Let => {
+                                try self.parseLet();
+                                break;
+                            },
+                            .Binary => {
+                                try self.parseBinary();
+                                break;
+                            },
+                            .Int => {
+                                try self.parseInt();
+                                break;
+                            },
+                            else => |k| {
+                                log.err("Not implemented for kind {any}", .{k});
+                                break;
+                            },
+                        }
+                    }
+                },
+                else => |err| {
+                    log.err("parseTerm - expected string, found {any}", .{err});
+                    return Error.ParserError;
+                },
+            }
         }
     }
 
     pub fn next(self: *ASTParser) !bool {
         const token = try self.source.next();
         switch (token) {
-            .string => |str| {
-                switch (self.state) {
-                    .root => {
-                        try self.parseRoot(str);
-                    },
-                    .parsing => {
-                        try self.parseTerm(str);
-                    },
+            .string => |s| {
+                if (std.mem.eql(u8, s, "expression")) {
+                    try self.parseTerm("expression");
+                } else if (std.mem.eql(u8, s, "location")) {
+                    try self.parseLoc();
                 }
             },
-
             .object_begin => {
                 return true;
             },
             .object_end, .end_of_document => return false,
             else => |key| {
-                log.debug("Found an invalid key {any}", .{key});
+                self.print("Found an invalid key {any}", .{key});
                 return false;
             },
         }
         return true;
     }
+
+    pub fn parse(allocator: Allocator, reader: std.fs.File.Reader) !void {
+        var timer = try std.time.Timer.start();
+
+        // The underlying reader, which already handles JSON
+        var source = std.json.reader(allocator, reader);
+
+        // To enable diagnostics, declare `var diagnostics = Diagnostics{};` then call `source.enableDiagnostics(&diagnostics);`
+        // where `source` is either a `std.json.Reader` or a `std.json.Scanner` that has just been initialized.
+        // At any time, notably just after an error, call `getLine()`, `getColumn()`, and/or `getByteOffset()`
+        // to get meaningful information from this.
+
+        var diagnostics = std.json.Diagnostics{};
+        source.enableDiagnostics(&diagnostics);
+        defer log.debug("Line: {d}, Column: {d}", .{ diagnostics.getLine(), diagnostics.getColumn() });
+
+        var parser = ASTParser{
+            .allocator = allocator,
+            .source = source,
+        };
+        while (try parser.next()) {
+            // self.print("Line: {d}, Column: {d}", .{ diagnostics.getLine(), diagnostics.getColumn() });
+        }
+
+        const elapsed = timer.read();
+        log.debug("ASTParser.parse: {}us", .{elapsed / 1000});
+    }
 };
+
+fn concat(allocator: Allocator, a: []const u8, b: []const u8) ![]u8 {
+    const result = try allocator.alloc(u8, a.len + b.len);
+    std.mem.copy(u8, result, a);
+    std.mem.copy(u8, result[a.len..], b);
+    return result;
+}
--- src/spec.zig
+++ src/spec.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const File = struct {
     name: []const u8,
     expression: Term,
@@ -18,9 +20,13 @@ pub const Term = union(enum) {
     tuple: *Tuple,
 };
 
-const Loc = struct { start: u32, end: u32, filename: []const u8 };
+pub const Loc = struct {
+    start: u32,
+    end: u32,
+    filename: []const u8,
+};
 
-const BinaryOp = enum {
+pub const BinaryOp = enum {
     Add,
     Sub,
     Mul,
@@ -36,21 +42,120 @@ const BinaryOp = enum {
     Or,
 };
 
-const Parameter = struct { text: []const u8, location: Loc };
+pub const Parameter = struct {
+    text: []const u8,
+    location: Loc,
+};
 
-pub const ValidKeys = enum { kind, condition, then, otherwise, name, next, callee, arguments, parameters, lhs, op, rhs, first, second, text, value, location, filename, start, end };
-pub const ValidTerms = enum { If, Let, Str, Bool, Int, Binary, Call, Function, Print, First, Second, Tuple, Var };
+pub const ValidKeys = enum {
+    kind,
+    condition,
+    then,
+    otherwise,
+    name,
+    next,
+    callee,
+    arguments,
+    parameters,
+    lhs,
+    op,
+    rhs,
+    first,
+    second,
+    text,
+    value,
+    location,
+    filename,
+    start,
+    end,
+};
+pub const ValidTerms = enum {
+    If,
+    Let,
+    Str,
+    Bool,
+    Int,
+    Binary,
+    Call,
+    Function,
+    Print,
+    First,
+    Second,
+    Tuple,
+    Var,
+};
 
-pub const If = struct { kind: []const u8, condition: Term, then: Term, otherwise: Term, location: Loc };
-pub const Let = struct { kind: []const u8, name: Parameter, value: Term, next: Term, location: Loc };
-pub const Str = struct { kind: []const u8, value: []const u8, location: Loc };
-pub const Bool = struct { kind: []const u8, value: bool, location: Loc };
-pub const Int = struct { kind: []const u8, value: i32, location: Loc };
-pub const Binary = struct { kind: []const u8, lhs: Term, op: BinaryOp, rhs: Term, location: Loc };
-pub const Call = struct { kind: []const u8, callee: Term, arguments: []Term, location: Loc };
-pub const Function = struct { kind: []const u8, parameters: []Parameter, value: Term, location: Loc };
-pub const Print = struct { kind: []const u8, value: Term, location: Loc };
-pub const First = struct { kind: []const u8, value: Term, location: Loc };
-pub const Second = struct { kind: []const u8, value: Term, location: Loc };
-pub const Tuple = struct { kind: []const u8, first: Term, second: Term, location: Loc };
-pub const Var = struct { kind: []const u8, text: []const u8, location: Loc };
+pub const If = struct {
+    kind: []const u8,
+    condition: Term,
+    then: Term,
+    otherwise: Term,
+    location: Loc,
+};
+pub const Let = struct {
+    kind: []const u8,
+    name: Parameter,
+    value: Term,
+    next: Term,
+    location: Loc,
+};
+pub const Str = struct {
+    kind: []const u8,
+    value: []const u8,
+    location: Loc,
+};
+pub const Bool = struct {
+    kind: []const u8,
+    value: bool,
+    location: Loc,
+};
+pub const Int = struct {
+    kind: []const u8,
+    value: i32,
+    location: Loc,
+};
+pub const Binary = struct {
+    kind: []const u8,
+    lhs: Term,
+    op: BinaryOp,
+    rhs: Term,
+    location: Loc,
+};
+pub const Call = struct {
+    kind: []const u8,
+    callee: Term,
+    arguments: []Term,
+    location: Loc,
+};
+pub const Function = struct {
+    kind: []const u8,
+    parameters: []Parameter,
+    value: Term,
+    location: Loc,
+};
+pub const Print = struct {
+    kind: []const u8,
+    value: Term,
+    location: Loc,
+};
+pub const First = struct {
+    kind: []const u8,
+    value: Term,
+    location: Loc,
+};
+pub const Second = struct {
+    kind: []const u8,
+    value: Term,
+    location: Loc,
+};
+pub const Tuple = struct {
+    kind: []const u8,
+    first: Term,
+    second: Term,
+    location: Loc,
+};
+pub const Var = struct {
+    kind: []const u8,
+    text: []const u8,
+    location: Loc,
+};
