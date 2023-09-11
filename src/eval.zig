eac57d513e1b1d4ae960f0795472f9624e47cb2b
dlopes7 <davidribeirolopes@gmail.com>
2023-09-11 14:45:28 -0500

    advance interpreter

--- src/eval.zig
+++ src/eval.zig
@@ -2,12 +2,9 @@ const std = @import("std");
 const spec = @import("spec.zig");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const print = std.debug.print;
 
-const SupportedValues = union(enum) {
-    int: i32,
-    str: []const u8,
-    bool: bool,
-};
+const SupportedValues = union(enum) { int: i32, str: []const u8, bool: bool };
 
 const Error = error{ EvalError, CompilerError, OutOfMemory };
 
@@ -23,34 +20,52 @@ fn evalParameter(param: spec.Parameter) []const u8 {
     return param.text;
 }
 
+fn evalTerms(terms: ArrayList(spec.Term), allocator: Allocator) Error!ArrayList(SupportedValues) {
+    var result = ArrayList(SupportedValues).init(allocator);
+    for (terms.items) |term| {
+        const value = try eval(term, allocator);
+        try result.append(value);
+    }
+    return result;
+}
+
 pub fn eval(term: spec.Term, allocator: Allocator) Error!SupportedValues {
     switch (term) {
         .function => |f| {
-            std.debug.print("eval function {any}\n", .{&f});
+            // print("eval function {any}\n", .{f});
             const parameters = try evalParameters(f.parameters, allocator);
             _ = parameters;
             const value = try eval(f.value, allocator);
             _ = value;
+
+            // print("Function - (", .{});
+            // for (parameters.items) |param| {
+            //     print(" {s} ", .{param});
+            // }
+            // print(")\n", .{});
+
             return SupportedValues{ .str = "function" };
         },
         .let => |l| {
-            std.debug.print("eval let {any}\n", .{l});
+            print("eval let {any}\n", .{l});
             const name = evalParameter(l.name);
             _ = name;
             const value = try eval(l.value, allocator);
             _ = value;
             const next = try eval(l.next, allocator);
             _ = next;
+            // print("Let - {s}({any}) -> {any}", .{ name, value, next });
             return SupportedValues{ .str = "let" };
         },
         .ifTerm => |v| {
-            std.debug.print("eval if {any}\n", .{v});
+            print("eval if {any}\n", .{v});
         },
         .varTerm => |v| {
+            print("eval var {any}\n", .{v});
             return SupportedValues{ .str = v.text };
         },
         .binary => |bin| {
-            std.debug.print("eval binary {any}\n", .{bin});
+            print("eval binary {any}\n", .{bin});
 
             switch (bin.op) {
                 .Add => {
@@ -88,44 +103,62 @@ pub fn eval(term: spec.Term, allocator: Allocator) Error!SupportedValues {
                                 },
                                 else => |r| {
                                     // l is str, r is unsupported
-                                    std.debug.print("Compiler error:  cannot perform {any} on {any} and {any}\n", .{ bin.op, l, r });
+                                    print("Compiler error:  cannot perform {any} on {any} and {any}\n", .{ bin.op, l, r });
                                     return Error.CompilerError;
                                 },
                             }
                         },
                         else => {
-                            std.debug.print("Compiler error: cannot perform {any} on {any} and {any}\n", .{ bin.op, left, right });
+                            print("Compiler error: cannot perform {any} on {any} and {any}\n", .{ bin.op, left, right });
                             return Error.CompilerError;
                         },
                     }
                 },
 
                 else => {
-                    std.debug.print("unsupported binary op {any}\n", .{bin.op});
+                    print("unsupported binary op {any}\n", .{bin.op});
                 },
             }
         },
         .int => |v| {
-            std.debug.print("eval int {any}\n", .{v});
+            print("eval int {any}\n", .{v});
             return SupportedValues{ .int = v.value };
         },
         .str => |v| {
             return SupportedValues{ .str = v.value };
         },
         .boolean => |v| {
-            std.debug.print("eval boolean {any}\n", .{v});
+            print("eval boolean {any}\n", .{v});
         },
-        .call => |v| {
-            std.debug.print("eval call {any}\n", .{v});
+        .call => |c| {
+            print("eval call {any}\n", .{c});
+            const callee = try eval(c.callee, allocator);
+            _ = callee;
+            const arguments = try evalTerms(c.arguments, allocator);
+            _ = arguments;
+            return SupportedValues{ .str = "call" };
         },
-        .print => |v| {
-            const value = try eval(l.value, allocator);
-            _ = value;
+        .print => |p| {
+            // print("eval print {any}\n", .{p});
+            const value = try eval(p.value, allocator);
+            switch (value) {
+                .int => |v| {
+                    print("compiler: {d}\n", .{v});
+                },
+                .str => |v| {
+                    print("compiler: {s}\n", .{v});
+                },
+                else => {
+                    print("compiler: {any}\n", .{value});
+                },
+            }
+
+            return SupportedValues{ .str = "print" };
         },
         .tuple => |v| {
-            std.debug.print("eval tuple {any}\n", .{v});
+            print("eval tuple {any}\n", .{v});
         },
     }
-    std.debug.print("EVAL - unsupported term {any}\n", .{term});
+    print("EVAL - unsupported term {any}\n", .{term});
     return Error.EvalError;
 }
